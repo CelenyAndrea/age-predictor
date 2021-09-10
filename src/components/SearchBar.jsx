42e6861766e598503d0a95a76b0e935300fa07f7
CelenyAndrea <77702240+CelenyAndrea@users.noreply.github.com>
2021-09-10 12:36:43 -0500

    input

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,10 +3,11 @@ import styles from '../components/nav/nav.module.css'
 
 export default function SearchBar({onSearch}) {
   const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
   return (
     <form onSubmit={(e) => {
       e.preventDefault();
-      onSearch(name);
+      onSearch(name, location);
       setName("");
     }}>
       <h2>Puede predecir la edad de un solo nombre o de varios</h2>
@@ -15,13 +16,14 @@ export default function SearchBar({onSearch}) {
         placeholder="Nombre..." 
         value={name}
         onChange={e => setName(e.target.value)}
+        required
       />
-      {/* <input
+      <input
         type="text" 
         placeholder="Localización..." 
-        value={name}
-        onChange={e => setName(e.target.value)}
-      /> */}
+        value={location}
+        onChange={e => setLocation(e.target.value)}
+      />
       <input className={styles.boton} type="submit" value="Predecir" />
     </form>
   );
